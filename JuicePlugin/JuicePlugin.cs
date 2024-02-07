bae163bd8074c86a6c4719f8adb64a5d7e54d912
vibeside <vibeside01@gmail.com>
2024-02-06 23:11:04 -0500

    first commit

---
+++ JuicePlugin/JuicePlugin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace JuicePlugin
+{
+    public class JuicePlugin
+    {
+        public void Awake()
+        {
+            Hook startOfRoundHook = new(
+            typeof(PlayerGlide).GetMethod(nameof(PlayerGlide.Update), (BindingFlags)int.MaxValue),
+            (Action<HUDManager> original, HUDManager self) =>
+            {
+                StartCoroutine(WaitUntilPostProcessApi());
+                original(self);
+            });
+        }
+    }
+}
